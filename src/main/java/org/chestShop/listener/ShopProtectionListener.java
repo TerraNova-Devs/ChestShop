0100c97fc8f2750f6e533747ace8574891f754dc
derbxsti <contact@bastitime.dev>
2024-12-11 17:44:11 +0100

    Fix breaking chest bug

--- src/main/java/org/chestShop/listener/ShopProtectionListener.java
+++ src/main/java/org/chestShop/listener/ShopProtectionListener.java
@@ -14,6 +14,7 @@ import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.vehicle.VehicleMoveEvent;
 import org.bukkit.event.world.StructureGrowEvent;
+import org.bukkit.inventory.DoubleChestInventory;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.chestShop.ChestShop;
@@ -53,13 +54,39 @@ public class ShopProtectionListener implements Listener {
                     Chat.sendErrorMessage(player, "You do not own this shop.");
                 }
             }
-        } else if (block.getState() instanceof Chest chest) {
-            if (isShopChest(chest)) {
-                Player player = event.getPlayer();
-                if (!isShopOwner(chest, player)) {
-                    event.setCancelled(true);
-                    Chat.sendErrorMessage(player, "You do not own this shop.");
+        }
+        else if (block.getState() instanceof Chest chest) {
+            boolean hasShopSign;
+            if (chest.getInventory() instanceof DoubleChestInventory) {
+                DoubleChest doubleChest = (DoubleChest) chest.getInventory().getHolder();
+                Chest leftChest = (Chest) doubleChest.getLeftSide();
+                Chest rightChest = (Chest) doubleChest.getRightSide();
+
+                hasShopSign = isShopSignAttached(leftChest) || isShopSignAttached(rightChest);
+            } else {
+                hasShopSign = isShopSignAttached(chest);
+            }
+
+            if (!hasShopSign) {
+                return;
+            }
+
+            Player player = event.getPlayer();
+            boolean isOwner = isOwner(chest, player);
+
+            if (chest.getInventory() instanceof DoubleChestInventory) {
+                DoubleChest doubleChest = (DoubleChest) chest.getInventory().getHolder();
+                Chest leftChest = (Chest) doubleChest.getLeftSide();
+                Chest rightChest = (Chest) doubleChest.getRightSide();
+
+                if (!isOwner) {
+                    isOwner = isOwner(leftChest, player) || isOwner(rightChest, player);
                 }
+            }
+
+            if (!isOwner) {
+                event.setCancelled(true);
+                Chat.sendErrorMessage(player, "Du besitzt diesen Shop nicht!");
             }
         }
     }
@@ -138,9 +165,16 @@ public class ShopProtectionListener implements Listener {
         }
     }
 
-    private boolean isShopSign(Sign sign) {
-        PersistentDataContainer data = sign.getPersistentDataContainer();
-        return data.has(new NamespacedKey(plugin, "shopItem"));
+    private boolean isOwner(Chest chest, Player player) {
+        Sign attachedSignBlock = getAttachedSign(chest.getBlock());
+        if (attachedSignBlock == null || !isShopSign(attachedSignBlock)) {
+            return false;
+        }
+
+        PersistentDataContainer data = attachedSignBlock.getPersistentDataContainer();
+        UUID ownerUUID = UUID.fromString(data.get(new NamespacedKey(plugin, "owner"), PersistentDataType.STRING));
+
+        return player.getUniqueId().equals(ownerUUID);
     }
 
     private boolean isShopChest(Chest chest) {
@@ -161,6 +195,39 @@ public class ShopProtectionListener implements Listener {
             }
         }
         return false;
+    }
+
+    private boolean isShopSignAttached(Chest chest) {
+        Sign attachedSignBlock = getAttachedSign(chest.getBlock());
+        return attachedSignBlock != null && isShopSign(attachedSignBlock);
+    }
+
+    private Sign getAttachedSign(Block chestBlock) {
+        Block[] possibleSignPositions = {
+                chestBlock.getRelative(1, 0, 0),
+                chestBlock.getRelative(-1, 0, 0),
+                chestBlock.getRelative(0, 0, 1),
+                chestBlock.getRelative(0, 0, -1)
+        };
+
+        for (Block block : possibleSignPositions) {
+            if (block.getState() instanceof Sign sign && sign.getBlockData() instanceof org.bukkit.block.data.type.WallSign) {
+                org.bukkit.block.data.type.WallSign wallSign = (org.bukkit.block.data.type.WallSign) sign.getBlockData();
+                Block attached = block.getRelative(wallSign.getFacing().getOppositeFace());
+                if (attached.equals(chestBlock)) {
+                    return sign;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private boolean isShopSign(Sign sign) {
+        if (sign == null)
+            return false;
+        PersistentDataContainer data = sign.getPersistentDataContainer();
+        return data.has(new NamespacedKey(plugin, "shopItem"));
     }
 
     private boolean isShopOwner(Sign sign, Player player) {
